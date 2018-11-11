cfb878d1452450abc4151e3d43290610be504f66
HJ <hersheyj32@gmail.com>
2018-11-11 09:46:02 -0800

    okay that seems like it worked

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,5 @@
-Book.create(
-    {"id" => 1,
-     "title" => "Try the Book",
-     "author" => "Hartl",
-     "desc" => "https://www.railstutorial.org/book/toy_app",
-     "created_at" => DateTime.parse("Sat, 10 Nov 2018 22:57:42 UTC +00:00)"),
-     "updated_at" => DateTime.parse("Sat, 10 Nov 2018 22:57:42 UTC +00:00}")})
-
-Book.create(
-    {"id" => 2,
-     "title" => "Girly Girl books",
-     "author" => "Hanton",
-     "desc" => "https://www.amazon.com/Princess-123s-Girly-Girl-Books/dp/1581179286",
-     "created_at" => DateTime.parse("Sat, 10 Nov 2018 22:57:42 UTC +00:00)"),
-     "updated_at" => DateTime.parse("Sat, 10 Nov 2018 22:57:42 UTC +00:00}")})
-
-#For the bookmarks
-bookmarks =
-    [{"id" => 1, "note" => "Just a test book mark", "book_id" => 1, "entry_date" => DateTime.parse("Sat, 10 Nov 2018"), "page" => 1, "progess" => 2, "created_at" => DateTime.parse("Sat, 10 Nov 2018 22:53:17 UTC +00:00"), "updated_at" => DateTime.parse("Sat, 10 Nov 2018 22:53:17 UTC +00:00")},
-
-     {"id" => 2, "note" => "Another book mark", "book_id" => 2, "entry_date" => DateTime.parse("Sat, 10 Nov 2018"), "page" => 1, "progess" => 2, "created_at" => DateTime.parse("Sat, 10 Nov 2018 22:53:17 UTC +00:00"), "updated_at" => DateTime.parse("Sat, 10 Nov 2018 22:53:17 UTC +00:00")}]
-
-bookmarks.each do |bookmark|
-  Bookmark.create(bookmark)
-end
+Book.create({"id"=>1, "title"=>"Try the Book", "author"=>"Hartl", "desc"=>"https://www.railstutorial.org/book/toy_app", "created_at"=>"DateTime.parse(\"2018-11-10 22:57:42 UTC\")", "updated_at"=>"DateTime.parse(\"2018-11-11 17:10:03 UTC\")", "total_page"=>100})
+Book.create({"id"=>2, "title"=>"Girly Girl books", "author"=>"Hanton", "desc"=>"https://www.amazon.com/Princess-123s-Girly-Girl-Books/dp/1581179286", "created_at"=>"DateTime.parse(\"2018-11-10 22:57:42 UTC\")", "updated_at"=>"DateTime.parse(\"2018-11-11 17:10:11 UTC\")", "total_page"=>100})
+Bookmark.create({"id"=>1, "note"=>"Just a test book mark", "book_id"=>1, "entry_date"=>"DateTime.parse(\"2018-11-10\")", "page"=>1, "progess"=>2, "created_at"=>"DateTime.parse(\"2018-11-10 22:53:17 UTC\")", "updated_at"=>"DateTime.parse(\"2018-11-10 22:53:17 UTC\")"})
+Bookmark.create({"id"=>2, "note"=>"Another book mark", "book_id"=>2, "entry_date"=>"DateTime.parse(\"2018-11-10\")", "page"=>1, "progess"=>2, "created_at"=>"DateTime.parse(\"2018-11-10 22:53:17 UTC\")", "updated_at"=>"DateTime.parse(\"2018-11-10 22:53:17 UTC\")"})
+Bookmark.create({"id"=>3, "note"=>"A second progress", "book_id"=>1, "entry_date"=>"DateTime.parse(\"2018-11-11\")", "page"=>2, "progess"=>3, "created_at"=>"DateTime.parse(\"2018-11-11 17:15:37 UTC\")", "updated_at"=>"DateTime.parse(\"2018-11-11 17:15:37 UTC\")"})
