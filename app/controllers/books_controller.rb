3b67d77595362b5f63109a30ca610e41f548ca43
HJ <hersheyj32@gmail.com>
2018-11-10 22:53:32 -0800

    this is really nice, finally got the progress to show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,6 +10,11 @@ class BooksController < ApplicationController
   # GET /books/1
   # GET /books/1.json
   def show
+    set_book
+    cur_bookmarks = @book.bookmarks
+    pages_read = cur_bookmarks.max_by {|m| m[:page]}.page
+    progress = (pages_read.to_f / @book.total_page.to_f)
+    @current_progress = progress
   end
 
   # GET /books/new
@@ -28,11 +33,11 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to @book, notice: 'Book was successfully created.' }
-        format.json { render :show, status: :created, location: @book }
+        format.html {redirect_to @book, notice: 'Book was successfully created.'}
+        format.json {render :show, status: :created, location: @book}
       else
-        format.html { render :new }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
+        format.html {render :new}
+        format.json {render json: @book.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -42,11 +47,11 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
-        format.json { render :show, status: :ok, location: @book }
+        format.html {redirect_to @book, notice: 'Book was successfully updated.'}
+        format.json {render :show, status: :ok, location: @book}
       else
-        format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @book.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -56,8 +61,8 @@ class BooksController < ApplicationController
   def destroy
     @book.destroy
     respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to books_url, notice: 'Book was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
@@ -68,13 +73,14 @@ class BooksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_book
-      @book = Book.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def book_params
-      params.require(:book).permit(:title, :author, :desc, :total_page)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def book_params
+    params.require(:book).permit(:title, :author, :desc, :total_page)
+  end
 end
